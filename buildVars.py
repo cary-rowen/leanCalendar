4fcc1a03eb4df98f3b00c2f58f7f9272324c6c30
cary-rowen <manchen_0528@outlook.com>
2026-06-21 11:01:39 +0800

    Set add-on version to 0.2.3

--- buildVars.py
+++ buildVars.py
@@ -30,7 +30,7 @@ addon_info = AddonInfo(
 		"""Reports Chinese lunar date, solar terms, Gregorian date and time details, and an interactive calendar query dialog.""",
 	),
 	# version
-	addon_version="0.2.2",
+	addon_version="0.2.3",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
 	addon_changelog=_(
